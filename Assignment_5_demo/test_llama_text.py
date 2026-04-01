2208e6d659787a93af99924170d287acfaef1c8b
MinHyuk Park <dmz44@txstate.edu>
2026-03-31 20:36:10 -0500

    Update test_llama_text.py

--- Assignment_5_demo/test_llama_text.py
+++ Assignment_5_demo/test_llama_text.py
@@ -1,25 +1,41 @@
-#!/usr/bin/env python3
+# Demo Code for Robotics_Assignment_5
+# Copyright: 2026 CS 4379K / CS 5342 Introduction to Autonomous Robotics, Robotics and Autonomous Systems
+
+# Natural Language Processing Code for Assignment 5.
+
+# This code will give you an introduction to integrate natural language pipeline to Python programming and allow you to finetune parameters outside ROS2 influence. This code will run as-is.
+
+# Refer to the Lab PowerPoint materials and Appendix of Assignment 3 to learn more about coding on ROS2 and the hardware architecture of Turtlebot3.
+# You need to run this code on the Remote-PC docker image.
+# You would need a basic understanding of Python Data Structure and Object Oriented Programming to understand this code.
+
 import os
 from llama_cpp import Llama
 
-# Simple Llama example.
+# Simple Llama Text to Text example.
 
-MODEL_PATH = os.path.expanduser("~/llama.cpp/models/llama-2-7b-32k-instruct.Q4_K_M.gguf")
-#MODEL_PATH = os.path.expanduser("~/llama.cpp/models/llama-2-7b-chat.Q4_K_M.gguf")
+##############################################################################
+# HINT: Modify MODEL_PATH for instruct or chat model.
+MODEL_PATH = os.path.expanduser("~/my_code/Robotics_Assignment_5/Assignment_5_demo/llama-2-7b-32k-instruct.Q4_K_M.gguf")
+#MODEL_PATH = os.path.expanduser("~/my_code/Robotics_Assignment_5/Assignment_5_demo/llama-2-7b-chat.Q4_K_M.gguf")
+##############################################################################
 
 def main():
     if not os.path.isfile(MODEL_PATH):
         print(f"ERROR: model not found at {MODEL_PATH}")
         return
-
+    ##############################################################################
+    # Optional: Feel free to modify these parameters to adjust performance of the LLM.
     llm = Llama(
         model_path=MODEL_PATH,
         n_ctx=512,
         n_threads=4,
         n_gpu_layers=33,
-        seed=42,
+        seed=420,
         use_mlock=True
     )
+    ##############################################################################
+    
     print("Model loaded. Type 'exit' to quit.")
 
     while True:
@@ -27,8 +43,10 @@ def main():
         if prompt.strip().lower() in ("exit", "quit"):
             break
 
-        print("Assistant: ", end="", flush=True)
-        full_reply = "" # Leaving it unused in this example.
+        print("Large Language Model: ", end="", flush=True)
+        full_reply = "" 
+        ##############################################################################
+        # Optional: Feel free to modify these parameters to adjust performance of the LLM.
         for chunk in llm(
             prompt=prompt,
             max_tokens=128,
@@ -38,11 +56,12 @@ def main():
             top_p=0.95,
             stream=True
         ):
+        ##############################################################################
             token = chunk["choices"][0]["text"]
             full_reply += token
             print(token, end="", flush=True)
+            
         print()  
 
 if __name__ == "__main__":
     main()
-
